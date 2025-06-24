284d1dd8e1d228f7bcc2fe6cdb4c4e1eca6f9f6f
odinOnGit <tezaschhetri@gmail.com>
2025-06-24 15:39:08 +0530

    Unique id system for Cards

--- front-end/src/components/CardList.jsx
+++ front-end/src/components/CardList.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import Card from './Card'
 import './CardList.css'
 
 function CardList() {
-  const cards = [1, 2, 3, 4]
+  const [cards, setCards] = useState([
+    {id:1},
+    {id:2},
+    {id:3},
+  ])
+
+  const handleAddCard = () => {
+    const newId = cards.length > 0 ? cards[cards.length - 1].id + 1 : 1
+    const newCard = { id: newId }
+    setCards([...cards, newCard])
+  }
 
   return (
     <div className='card-list'>
-      {cards.map((id) => (
-        <Card key={id} id={id}></Card>
-      ))}
+      <button onClick={handleAddCard}>Add Card</button>
+      {cards.map((card) => (
+        <Card key={card.id} id={card.id} />
+      ))
+      }
     </div>
   )
 }
