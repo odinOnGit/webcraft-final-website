8fce2e6f7734481277e3a4c8f317886c53631e4c
odinOnGit <tezaschhetri@gmail.com>
2025-06-28 11:11:37 +0530

    Mentors Page

--- front-end/src/components/Mentors.jsx
+++ front-end/src/components/Mentors.jsx
@@ -1,9 +1,47 @@
 import './Mentors.css'
 
 function Mentor() {
+
+  let MentorCards = [
+    {
+      id: 1,
+      title: 'Anurag',
+      number: 7499972586,
+      description: 'alala',
+      thumbnail: 'photo.png'
+    },
+    {
+      id: 2,
+      title: 'Tezas',
+      number: 7499972586,
+      description: 'alala',
+      thumbnail: 'photo.png'
+    },
+    {
+      id: 3,
+      title: 'Nitansh',
+      number: 7499972586,
+      description: 'alala',
+      thumbnail: 'photo.png'
+    },
+  ];
+
   return (
     <section id='home' className='mentor-section'>
-      <h2>Mentors Page</h2>
+      <h2 className="title">Mentors Page</h2>
+
+      <div className="card-list">
+        {MentorCards.map(({ id, title, number, description, thumbnail }) => (
+          <div key={id} className="mentor-card">
+            {thumbnail && <img src={thumbnail} alt={`${title} thumbnail`} className="card-thumbnail" />}
+            <div className="card-info">
+              <h3>{title}</h3>
+              <p className="number">{number}</p>
+              <p className="description">{description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
